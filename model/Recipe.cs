4c1796bee6d0834c111b1d77f79bbfe89552434f
David Román <43916038+MrDave1999@users.noreply.github.com>
2019-11-02 16:24:16 -0500

    Recipe

---
+++ model/Recipe.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ProgramRecipe
+{
+    class Recipe
+    {
+        public string Drugs { get; set; }
+
+        public string Prescription { get; set; }
+
+        public string DateBegin { get; set; }
+
+        public string DateDest { get; set; }
+    }
+}
