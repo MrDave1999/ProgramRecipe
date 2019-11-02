3977a3679c86b192071cfb1f738a1d458700ce93
David Román <43916038+MrDave1999@users.noreply.github.com>
2019-11-02 16:23:51 -0500

    Patient

---
+++ model/Patient.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ProgramRecipe
+{
+    class Patient
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Idcard { get; set; }
+        public string Age { get; set; }
+    }
+}
