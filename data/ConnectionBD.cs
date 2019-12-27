e4934d4a4652f1764c3c69f1a244e422acf6b0c1
David Román <43916038+MrDave1999@users.noreply.github.com>
2019-12-27 13:27:11 -0500

    File Config for connectionString

--- data/ConnectionBD.cs
+++ data/ConnectionBD.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using System.Text;
 using System.Data.OleDb;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace ProgramRecipe
 {
     static class ConnectionBD
     {
         public static OleDbConnection Connection { get; set; }
+
+        public static string getConectionString()
+        {
+            var provider = ConfigurationManager.ConnectionStrings["Recipe"].ProviderName;
+            var csSetting = ConfigurationManager.ConnectionStrings["Recipe"].ConnectionString;
+            return "Provider =" + provider + "Data Source = " + csSetting; 
+        }
+
 		public static bool Open()
 		{
 			try
 			{
                 Connection = new OleDbConnection();
-                Connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\dave\Documents\dateRecipe.accdb";
+                Connection.ConnectionString = getConectionString();
                 Connection.Open();
+                //Console.Write("Conexión satisfactoria!");
 			}
 			catch (Exception e)
 			{
@@ -26,4 +36,3 @@ namespace ProgramRecipe
 		}
 	}
 }
- 
